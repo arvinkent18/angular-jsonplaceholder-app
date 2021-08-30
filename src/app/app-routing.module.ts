af2f1da02ed097031d4ac91a38cbd02bafb40e66
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2021-08-30 11:09:48 +0800

    Added Photos Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'photos', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
